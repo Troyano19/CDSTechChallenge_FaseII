1ce78cfe9e3e92a42e71bee560346d1124444a6e
Javier Troyano Torrado <javitt333@gmail.com>
2025-03-09 15:18:21 +0100

    Prueba vercel 1

--- src/app.js
+++ src/app.js
@@ -6,22 +6,18 @@ const path = require('path');
 const frontend = require('./routes/frontendRouter')
 //We configure the use of dotenv for variables
 require('dotenv').config();
-const startWebServer = async () => {
-    try{
-        //We inicialize the express app
-        const app = express();
-        const port = process.env.PORT;
-        //Load the middleware that analize the body of the requests
-        app.use(express.json());
-        //Load the routers
-        app.use(frontend)
-        app.listen(port, () => {
-            console.log(`Web server listening on port ${port}`);
-        });
 
-    }catch(e){
-        console.log(`Ha ocurrido un error inesperado: ${e}`);
-    }
-}
-console.log("Iniciando servidor web");
-startWebServer();
+//We inicialize the express app
+const app = express();
+const port = process.env.PORT;
+//Load the middleware that analize the body of the requests
+app.use(express.json());
+//Load the routers
+app.use(frontend);
+
+
+app.listen(port, () => {
+    console.log(`Web server listening on port ${port}`);
+});
+
+module.exports = app;
