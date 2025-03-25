cc2a70b5b392411f0bb206899f69858da5a0b603
Javier Troyano Torrado <javitt333@gmail.com>
2025-03-25 22:01:19 +0100

    Añadido modelo de rutas

---
+++ api/models/trailModel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const trailModel = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    difficulty: {
+        type: String,
+        enum:  ["Newby", "Easy", "Medium", "Hard" ],
+        required: true,
+    },
+    images: {
+        banner: {
+            type: String,
+        },
+        gallery: {
+            type: [String],
+        },
+    },
+    tags: {
+        type: [String],
+        enum: ["Mountain", "Beach", "City", "Forest", "Desert", "Jungle", "Tundra", "family", "climb", "bike", "walk", "run"],
+    },
+    description: {
+        type: String,
+    },
+    location:{
+        start: {
+            type: [Number],
+            index: '2dsphere',
+            required: true,
+        },
+        end: {
+            type: [Number],
+            index: '2dsphere',
+            required: true,
+        },
+    }
+});
