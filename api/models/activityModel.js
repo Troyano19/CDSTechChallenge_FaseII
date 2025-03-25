583e0ec9b71aa145e0b4a3ff0c1d15298f509057
Javier Troyano Torrado <javitt333@gmail.com>
2025-03-25 22:07:01 +0100

    Añadido modelo de actividades

---
+++ api/models/activityModel.js
@@ -0,0 +1,68 @@
+const mongoose = require('mongoose');
+
+const activitySchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    type: {
+        type: String,
+        enum: ["Aire libre", "Cultura", "Deportes", "Gastronomía", "Relax"],
+        required: true,
+    },
+    location: {
+        type: [Number],
+        index: '2dsphere',
+        required: true,
+    },
+    images: {
+        banner: {
+            type: String,
+        },
+        gallery: {
+            type: [String],
+        },
+    },
+    created_at: {
+        type: Date,
+        default: Date.now,
+    },
+    updated_at: {
+        type: Date,
+        default: Date.now,
+    },
+    schedule: {
+        monday: [{
+            open: { type: String, required: true },
+            close: { type: String, required: true }
+        }],
+        tuesday: [{
+            open: { type: String, required: true },
+            close: { type: String, required: true }
+        }],
+        wednesday: [{
+            open: { type: String, required: true },
+            close: { type: String, required: true }
+        }],
+        thursday: [{
+            open: { type: String, required: true },
+            close: { type: String, required: true }
+        }],
+        friday: [{
+            open: { type: String, required: true },
+            close: { type: String, required: true }
+        }],
+        saturday: [{
+            open: { type: String, required: true },
+            close: { type: String, required: true }
+        }],
+        sunday: [{
+            open: { type: String, required: true },
+            close: { type: String, required: true }
+        }],
+    },
+});
